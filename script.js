1b9b48fd459d38d617ab5c15f28b01f76bca943a
bgman01 <b.gminski01@gmail.com>
2022-04-16 11:33:50 -0700

    Build initial UI with some CSS. Add images for RPS. Game fully functional with reset button.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = 0
+let computerScore = 0
 
-const optionBtn = document.querySelectorAll('div.optionBtn button')
-const buttons = document.querySelectorAll("#btn")
-
+const buttons = document.querySelectorAll('.button')
+const main = document.querySelector('main')
 
 function computerPlay() {
     let array = ['rock', 'paper', 'scissors']
@@ -11,77 +10,75 @@ function computerPlay() {
     return computerPlay;
 }
 
-
 function playRound(playerSelection,computerSelection){
     if (playerSelection === "rock") {
             if (computerSelection === "rock") {
-            document.getElementById('welcome').innerText = ("Draw!");
+            document.getElementById('message').innerText = "Draw!";
         } else if (computerSelection === "paper") {
             computerScore++
-            document.getElementById('computerScoreDisplay').innerText = computerScore;
-            document.getElementById('welcome').innerText = ("You lose! Paper beats Rock")
+            document.getElementById('computer-score').innerText = computerScore;
+            document.getElementById('message').innerText = "You lose! Paper beats Rock"
         } else if (computerSelection === "scissors") {
             playerScore++;
-            document.getElementById('playerScoreDisplay').innerText = playerScore;
-            document.getElementById('welcome').innerText = ("You Win! Rock beats Scissors!")
+            document.getElementById('player-score').innerText = playerScore;
+            document.getElementById('message').innerText = "You Win! Rock beats Scissors!"
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
             playerScore++;
-            document.getElementById('playerScoreDisplay').innerText = playerScore;
-            document.getElementById('welcome').innerText = ("You Win! Paper beats Rock!")
+            document.getElementById('player-score').innerText = playerScore;
+            document.getElementById('message').innerText = "You Win! Paper beats Rock!"
         } else if (computerSelection === "paper") {
-            document.getElementById('welcome').innerText = ("Draw!");
+            document.getElementById('message').innerText = "Draw!";
         } else if (computerSelection === "scissors") {
             computerScore++;
-            document.getElementById('computerScoreDisplay').innerText = computerScore;
-            document.getElementById('welcome').innerText = ("You lose! Scissors beats Paper")
+            document.getElementById('computer-score').innerText = computerScore;
+            document.getElementById('message').innerText = "You lose! Scissors beats Paper"
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
             playerScore++
-            document.getElementById('playerScoreDisplay').innerText = playerScore;
-            document.getElementById('welcome').innerText = ("You Win! Scissors beats Paper!")
+            document.getElementById('player-score').innerText = playerScore;
+            document.getElementById('message').innerText = "You Win! Scissors beats Paper!"
         } else if (computerSelection === "rock") {
             computerScore++;
-            document.getElementById('computerScoreDisplay').innerText = computerScore;
-            document.getElementById('welcome').innerText = ("You lose! Rock beats Scissors")
+            document.getElementById('computer-score').innerText = computerScore;
+            document.getElementById('message').innerText = "You lose! Rock beats Scissors"
         } else if (computerSelection === "scissors") {
-            document.getElementById('welcome').innerText = ("Draw!");
+            document.getElementById('message').innerText = "Draw!";
                     }
                 }
             }
-                
-function game() {
-    buttons.forEach((button1) => {
-        button1.addEventListener('click', () => {
-            let computerSelection = computerPlay();
-            if (button1.id === "btn") {
-                playRound(button1.value, computerSelection)
-            }
-            if (playerScore === 5) {
-                document.getElementById('congrats').innerText = `You won the game! You scored ${playerScore } points and the computer scored ${computerScore } points!`
-            }
-            else if (computerScore === 5) {
-                document.getElementById('congrats').innerText `You lost the game! You scored ${playerScore } points and the computer scored ${computerScore } points!`
 
-            }
-            if (playerScore === 5 || computerScore === 5) {
-                buttons.forEach((button) => {
-                    button.removeAttribute("id")
-                })
-            }
+function game() {
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const img = button.querySelector('img');
+        playerSelection = img.alt;
+        let computerSelection = computerPlay()
+        playRound(playerSelection,computerSelection);
+
+        if (playerScore === 5) {
+            document.getElementById('congrats').innerText = `You won the game! You scored ${playerScore } points and the CPU scored ${computerScore } points!`
+        }
+        
+        else if (computerScore === 5) {
+            document.getElementById('congrats').innerText = `You lost the game! You scored ${playerScore } points and the CPU scored ${computerScore } points!`
+        }
+
         })
     })
+
 }
+
 function playAgain() {
     const playAgain = document.querySelector('#playAgain');
     playAgain.addEventListener('click', () => {
         computerScore = 0;
         playerScore = 0;
-        document.getElementById("playerScoreDisplay").innerText = playerScore;
-        document.getElementById("computerScoreDisplay").innerText = computerScore;
-        document.getElementById('welcome').innerText = "Click any of the buttons below to start playing again."
+        document.getElementById("player-score").innerText = playerScore;
+        document.getElementById("computer-score").innerText = computerScore;
+        document.getElementById('message').innerText = "Game has been reset. Make your choices again!"
         document.getElementById("congrats").innerText = ""
         buttons.forEach((button) => {
             button.setAttribute("id", "btn")
